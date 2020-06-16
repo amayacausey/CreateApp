edfc179cb1f52934610e74791b8e4df574c4a4a6
amayacausey <amayacausey@gmail.com>
2020-06-16 13:14:10 -0700

    first commit

---
+++ src/CreateApp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CreateApp {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Hey Mom and Dad! \n\nHere are some interesting details about the first country I'm visiting.");
+        String name = "Kenya";
+        String popSize="49.7 million";
+        String capital="Nairobi";
+        String currency="One U.S Dollar equals $103.84 Kenya Shillings!!";
+
+        System.out.println("\nName: " +name);
+        System.out.println("\nPopulation Size: " +popSize);
+        System.out.println("\nCapital: " +capital);
+        System.out.println("\nCurrency: " +currency);
+        System.out.println("\nI'm so excited to visit the Nairobi Nation Park and feel the giraffes!! Talk later.");
+
+
+
+    }
+}
